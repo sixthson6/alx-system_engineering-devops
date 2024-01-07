f7e6e9e7370d522d1e12a33e2b55734eced7cb34
sixthson6 <sudais2408@gmail.com>
2024-01-07 02:28:32 +0000

    parsing sms log

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-name1 = ARGV[0].scan(/(?<=from:)[a-zA-Z0-9]+/).join
-name2 = ARGV[0].scan(/(?<=to:)[+A-Za-z0-9]+/).join
-name3 = ARGV[0].scan(/(?<=flags:)[-:0-1]+/).join
-puts "#{name1},#{name2},#{name3}"
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
