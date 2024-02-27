ccceb4b7b90ed1b7176fede15d587d969d09e5f0
sixthson6 <sudais2408@gmail.com>
2024-02-27 00:15:45 +0000

    Add puppet configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,6 @@
 # Puppet to configure local ssh client
 
-file_line { 'disble passwd auth':
-ensure  => present,
-line    => 'PassowordAuthentication no',
-path    => '/etc/ssh/ssh_config',
-match   => '^\s*#?\s*PasswordAuthentication'
-}
-
-file_line { 'use private key for auth'
-ensure   => present,
-line     => 'IdentifyFile ~/.ssh/school',
-path     => '/etc/ssh/ssh_config',
-match    => '^\s*#?\s*IdentifyFile ~/.ssh/school'
+exec { 'Puppet Configuration':
+path    => '/usr/bin/:/bin',
+command => 'echo -e "Host 100.25.34.201\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config'
 }
