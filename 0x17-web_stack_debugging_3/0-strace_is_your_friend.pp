81f8089ea397b65e0bc9562130a0cbd3a94c6719
sixthson6 <sudais2408@gmail.com>
2024-05-11 22:33:27 +0000

    fix bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Debug error code 500 error when request using GET HTTP to Apache web server
 
-exec {'replace_in_config_file':
+exec {'replace':
 	provider => shell,
 	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
