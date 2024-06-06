406806f68e9cef252403dbff20c33845ef07ab10
sixthson6 <sudais2408@gmail.com>
2024-06-06 20:15:28 +0000

    Export todo list of all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Exports todo list information of all employees
+    to JSON format
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "username": u.get("username"),
+                "task": t.get("title"),
+                "completed": t.get("completed")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
