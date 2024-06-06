8f5bcaf5fe261a8a47069c72e87e03de9e63408b
sixthson6 <sudais2408@gmail.com>
2024-06-06 22:37:50 +0000

    Query number of subscribers on API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers
+   for a given subreddit
+   If an invalid subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    sub = requests.get(url)
+    if sub.status_code == 200:
+        subs = sub.json()
+        if 'data' in subs and 'subscribers' in subs['data']:
+            return subs['data']['subscribers']
+        else:
+            return
+    else:
+        return 0
