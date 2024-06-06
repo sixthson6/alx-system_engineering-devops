dbff2ef73370b47c6cd74a1037289b01c4f2d848
sixthson6 <sudais2408@gmail.com>
2024-06-06 22:42:10 +0000

    Query number of subscribers on API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    sub = requests.get(url)
+    sub = requests.get(url, allow_redirects=False)
     if sub.status_code == 200:
         subs = sub.json()
         if 'data' in subs and 'subscribers' in subs['data']:
