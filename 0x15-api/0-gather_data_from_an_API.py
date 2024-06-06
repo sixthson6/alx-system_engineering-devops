c3ab8486863571a704929c8f1483f810112700c7
sixthson6 <sudais2408@gmail.com>
2024-06-06 11:26:47 +0000

    todo list api retrieve

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,6 @@ todo list progress from an api
 if __name__ == "__main__":
     import requests
     import sys
-
-
     r = requests.get('https://jsonplaceholder.typicode.com/todos/')
     em_name = requests.get('https://jsonplaceholder.typicode.com/users/')
     if r.status_code == 200 and em_name.status_code == 200:
@@ -15,13 +13,13 @@ if __name__ == "__main__":
         name = em_name.json()
         count = 0
         for item in data:
-            if item['userId'] == id and item['completed'] == True:
+            if item['userId'] == id and item['completed'] is True:
                 count += 1
         for n in name:
             if n['id'] == id:
                 employee_name = n['name']
-       
-        print("Employee {} is done with tasks({}/20)".format(employee_name, count))
+        print("Employee {} is done with tasks({}/20)".format(employee_name,
+                                                             count))
         for item in data:
-            if item['userId'] == id and item['completed'] == True:   
+            if item['userId'] == id and item['completed'] is True:
                 print("\t{}".format(item['title']))
