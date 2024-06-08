d7e27d9e9930492b6bc9fda4d45f9e0ca0305f28
sixthson6 <sudais2408@gmail.com>
2024-06-08 02:59:55 +0000

    Query Reddit API for num of subscribers for given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python3:1-subs:v1.0.0 (by /u/000)"}
     sub = requests.get(url, headers=headers, allow_redirects=False)
     if sub.status_code == 200:
         subs = sub.json()
