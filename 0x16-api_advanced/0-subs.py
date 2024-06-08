8f14c3d76abf68f3a2231e4ddb1f05138a0e282a
sixthson6 <sudais2408@gmail.com>
2024-06-08 03:24:21 +0000

    Query Reddit API for num of subscribers for given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns the number of subscribers
+   for a given subreddit
+   If an invalid subreddit is given, the function should return 0.
+   """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "python3:1-subs:v1.0.0 (by /u/000)"}
     try:
