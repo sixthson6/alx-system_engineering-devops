6e57b41c118f85fa6d3bbc798e497d78576f6ce1
sixthson6 <108383344+sixthson6@users.noreply.github.com>
2024-07-01 03:40:52 +0000

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Debug error code 500 error when request using GET HTTP to Apache web server
-
-exec {'replace':
-	provider => shell,
-	command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
