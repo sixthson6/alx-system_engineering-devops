932d4f0000476bab00818f9bbdcbc17da0ecde35
sixthson6 <sudais2408@gmail.com>
2024-08-16 02:04:26 +0000

    Retrieve number of hot posts for a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@
 import requests
 # import sys
 
+
 def top_ten(subreddit):
     """
     Print first 10 hot posted listed for a given subreddit
@@ -12,13 +13,14 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = { 'limit': 10}
+    params = {'limit': 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url,
+                            headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data').get('children')
-        [print(post.get('data').get('title') for post in posts)]
+        data = response.json().get('data')
+        [print(c.get('data').get('title') for c in data.get('children'))]
     else:
         return 0
